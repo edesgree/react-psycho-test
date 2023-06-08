0222d9c0a2be638e25c0e16c7ada5cafe8e91870
Edouard Desgrée <edouard.desgree@gmail.com>
2023-06-08 08:39:56 +0200

    title of app

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import Intro from './components/Intro';
 import Quiz from './components/Quiz';
 
 function App() {
-  const [gameStart, setGameStart] = React.useState(false);
+  const [gameStart, setGameStart] = React.useState<boolean>(false);
   function startGame(): void {
     setGameStart(true);
   }
   return (
     <main className="App">
       <header>
-        <h1>Quizzy</h1>
+        <h1>Psy test</h1>
       </header>
-      {!gameStart && <Intro testt={33} start={startGame} />}
+      {!gameStart && <Intro start={startGame} />}
       {gameStart && <Quiz />}
     </main>
   );
