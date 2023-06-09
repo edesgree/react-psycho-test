1fce451eb972bbac0495bf820efc9f8b2a093b31
Edouard Desgrée <edouard.desgree@gmail.com>
2023-06-09 10:22:01 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ function App() {
                 initial={{ opacity: 0, x: '100%' }}
                 animate={{
                   opacity: 1,
-                  x: '00%'
+                  x: '0'
                 }}
                 transition={{ duration: 0.2, ease: 'easeOut' }}
               >
